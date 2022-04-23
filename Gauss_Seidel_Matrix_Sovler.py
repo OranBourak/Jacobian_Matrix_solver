753618c17e5af4ed11978011a9c51bfa7935a942
Tomer Burman <tomerbu1@ac.sce.ac.il>
2022-04-23 18:04:09 +0300

    Working gaus_seidel_solver added.

---
+++ Gauss_Seidel_Matrix_Sovler.py
@@ -0,0 +1,31 @@
+from Jacobian_numeric import *
+
+
+def gauss_seidel_solver(matrix,epsilon):
+    n, m = find_matrix_size(matrix)
+    maxLoops = None
+    if not rearangeDominantDiagonal(matrix):
+        print("Matrix is not diagonally dominant")
+        maxLoops = 100
+    equations = buildEquationsList(matrix) # [[0,1,2,3],[4,0,5,6],[7,8,0,9]], values[0,0,0]
+    values = [0 for x in range(m-1)]
+
+    while True:
+        if maxLoops != None:
+            maxLoops -=1
+        temp_list = list(values)
+        for i in range(m - 1): # []
+            values[i] = sum([values[j]*equations[i][j] for j in range(m-1)])
+            values[i] +=equations[i][-1]
+
+        print(values)
+        for i in range(m-1):
+            if abs(temp_list[i] - values[i]) <= epsilon:
+                print(values)
+                return values
+        if maxLoops == 0:
+            return
+
+
+gauss_seidel_solver([[4, 2, 0, 2], [2, 10, 4, 6], [0, 4, 5, 5]], 0.000001)
+
